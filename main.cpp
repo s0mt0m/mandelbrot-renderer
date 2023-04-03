cc8cbe76f02f0787ffb46b8c8a94973f4ef88525
Tomáš Krchňák <krchnak.tomi@gmail.com>
2023-04-04 00:55:59 +0200

    Add events and update main

--- main.cpp
+++ main.cpp
@@ -1,22 +1,43 @@
 #include <SDL.h>
 
 #include <iostream>
+#include <string_view>
 
+#include "mandelbrot.hpp"
 #include "sdl2/sdl_context.hpp"
 #include "sdl2/sdl_window.hpp"
 
 
 int main()
 {
-    sdl_context context;
+    constexpr std::string_view title( "Mandelbrot set visualizer" );
+    constexpr int width = 1280;
+    constexpr int height = 720;
 
-    sdl_window window( "test", 720, 540 );
+    sdl_context &context = sdl_context::get_context();
+    sdl_window window = context.create_window( title, width, height );
     sdl_renderer renderer = window.create_renderer();
 
+    mandelbrot mb( renderer, width, height );
+
+    SDL_Event event;
     while ( !context.should_quit() )
     {
-        renderer.set_colour( 0, 0, 0 );
-        renderer.clear();
+        while ( SDL_PollEvent( &event ) )
+        {
+            if ( event.type == SDL_MOUSEBUTTONDOWN )
+            {
+                if ( event.button.button == SDL_BUTTON_LEFT )
+                    mb.zoom_in( event.motion.x, event.motion.y );
+                else if ( event.button.button == SDL_BUTTON_RIGHT)
+                    mb.zoom_out( event.motion.x, event.motion.y );
+            }
+            else if ( event.type == SDL_WINDOWEVENT )
+            {
+                if ( event.window.event == SDL_WINDOWEVENT_EXPOSED )
+                    mb.resize( event.window.data1, event.window.data2 );
+            }
+        }
 
         renderer.render();
 
